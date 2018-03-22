4bf4785cd5adaec8ed7d1b3c89c220b68dbd6e95
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 13:26:16 -0600

    deletes boiler plate, starts fresh

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// create a new component to generate html 
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// display this component's generated html in the browser (DOM)
