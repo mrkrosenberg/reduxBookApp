8a4635c848e1b192562965effd0bf21f7f9ae89f
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 13:39:24 -0600

    adds content to div

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 // create a new component to generate html 
 const App = function() {
     // jsx - similar to html but is actually javascript: this is how transpilers turn javascript into html for the browser
-    return <div>Hello World!</div>; 
+    return  <div>Hello World!
+                <ol>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ol>
+            </div>; 
 }
 
 
 // display this component's generated html in the browser (DOM)
+
+
+
