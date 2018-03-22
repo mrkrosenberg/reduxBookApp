612277e73f6c093829f65fe48113f4d929b50feb
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 13:34:11 -0600

    lecture 5 commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // create a new component to generate html 
-
+const App = function() {
+    // jsx - similar to html but is actually javascript: this is how transpilers turn javascript into html for the browser
+    return <div>Hello World!</div>; 
+}
 
 
 // display this component's generated html in the browser (DOM)
