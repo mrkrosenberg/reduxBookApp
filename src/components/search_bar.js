598170f5298dca7f21ca309d877c89d7cf9ae539
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 14:49:23 -0600

    env variables added

---
+++ src/components/search_bar.js
@@ -0,0 +1,10 @@
+// Depenedencies
+import React from 'react';
+
+// define the component class
+const SearchBar = () => {
+    return <input type="text"/>;
+};
+
+// exports this component explicitly
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 // Dependencies
 import React from 'react';  // creates and managers components
 import ReactDOM from 'react-dom'; // used to interact with the actual DOM
+import API_KEY from './config/env';
+
+// component imports
+import SearchBar from './components/search_bar';
+
+// youTube API key
+const key = API_KEY;
 
 // create a new component to generate html 
 const App = () => {
     // jsx - similar to html but is actually javascript: this is how transpilers turn javascript into html for the browser
-    return  <div>Hello World!
-                <ol>
-                    <li>markDawg</li>
-                    <li>Tokki</li>
-                    <li>macDawg</li>
-                </ol>
-            </div>; 
+    return ( <div>
+                <SearchBar/>
+            </div> 
+    );
 }
 
 
