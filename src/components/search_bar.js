72d3512ce25287be0a1a46f4936e890fc8c306bf
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 15:25:39 -0600

    event handlers and state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,32 @@
 // Depenedencies
-import React from 'react';
+import React, { Component } from 'react';
 
 // define the component class
-const SearchBar = () => {
-    return <input type="text"/>;
-};
+// gives it the functionality of React.Component 
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // initializes the state of this component
+        this.state = { term: ''}; 
+    }
+
+    // every class needs a render method
+    render() {
+        // wrap js variables in curly braces
+        // creates a property -onChange- for this input element
+        return <input onChange={this.onInputChange} />;
+    };
+    // es6 syntax for cleaner code:
+    // onChange={(event) => console.log(event.target.value)};
+
+    // best practices for naming event handlers - 'on' or 'handle' before the name of the event
+    // triggered every time the input field is changed
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}  
 
 // exports this component explicitly
 export default SearchBar;
