6c82e550cbd5cac5ad9076d54dcaea0f6406fead
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-22 14:09:35 -0600

    builds basic app structure

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-// Imports
-import React from 'react';
+// Dependencies
+import React from 'react';  // creates and managers components
+import ReactDOM from 'react-dom'; // used to interact with the actual DOM
 
 // create a new component to generate html 
-const App = function() {
+const App = () => {
     // jsx - similar to html but is actually javascript: this is how transpilers turn javascript into html for the browser
     return  <div>Hello World!
                 <ol>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
+                    <li>markDawg</li>
+                    <li>Tokki</li>
+                    <li>macDawg</li>
                 </ol>
             </div>; 
 }
 
 
 // display this component's generated html in the browser (DOM)
-React.render(App);
+// requires the ReactDOM library
+// takes two arguments: the instantiated component class and the target for the render function
+ReactDOM.render(<App />, document.querySelector('.container'));
 
 
