a38b1a8392b4780fcab211d108301d65ad382c90
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-16 16:37:03 -0600

    builds reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+// creates the reducer 
+export default function() {
+
+    return [
+        { title : 'Teachings of Don Juan' },
+        { title: 'Harry Potter' },
+        { title: 'The Push' },
+        { title: 'Dirtbag' }
+    ]
+
+}
