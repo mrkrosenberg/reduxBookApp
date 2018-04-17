c051c1b7db14b0352b8c112d0a1a74f52e36b236
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-17 12:00:15 -0600

    adds comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+// imports the value returned by the reducer function from reducer_books.js
 import BooksReducer from './reducer_books'; 
 
 // groups all of the key-value pairs that we have in here into global state 
