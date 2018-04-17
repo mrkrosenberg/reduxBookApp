0627a0f5287f8856156d1218f33c09c249860383
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-17 11:19:16 -0600

    fixes dev server issue, fixes rendering issues

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 
-
-export default class BookList extends Component {
+class BookList extends Component {
 
     renderList() {
         return this.props.books.map((book) => {
@@ -13,10 +13,30 @@ export default class BookList extends Component {
     };
 
     render() {
-        <ul className="list-group col-sm-4">
-            {this.renderList()}
-        </ul>
+
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
 
     };
 
 };
+
+// takes application state as an argument
+// this function is the glue between React and Redux
+function mapStateToProps(state) {
+    // whatever object is returned from this function will show up as this.props 
+    // inside BookList (this container)
+
+    return {
+        // this is the connection between the container and the reducer
+        books: state.books
+    }
+
+}
+
+// takes a function and a component and produces a container
+// containers are components that are aware of state
+export default connect(mapStateToProps)(BookList);
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,6 @@ module.exports = {
     historyApiFallback: true,
     contentBase: './',
     inline: true,
-    port : 3000
+    port : 8080
   }
 };
