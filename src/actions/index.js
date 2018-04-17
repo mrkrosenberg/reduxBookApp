0ab8082183556ed15219037c6789287d9a47af96
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-17 16:49:38 -0600

    updates the action creator with an actual action w/ type and payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 // define an action creator
 export function selectBook(book) {
-    // selectBook is an action creator
+    // selectBook is an ActionCreator
     // it needs to return an action (an object with a type property)
     return {
-
+        type: 'BOOK_SELECTED',
+        payload: book
     }
 
 };
