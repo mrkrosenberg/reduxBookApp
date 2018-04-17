9da66dc2f452b8fe0993b85d59fde90b8753e8cd
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-04-17 11:45:25 -0600

    adds comments to clarify react-redux connection and process for accessing state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books'; 
 
+// groups all of the key-value pairs that we have in here into global state 
+// global state can be accessed by referencing "state"
 const rootReducer = combineReducers({
     // name of the piece of state and the value produced
+    // adds a key (books) to our global state
     books: BooksReducer
 });
 
